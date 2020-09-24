21a389b6f627ce132f2c4f3ef119d19df3f8737a
Sebastian Kann <sebastiankann@gmail.com>
2020-09-24 22:00:06 +0000

    added a testing file

--- breedFetcherRefactor.js
+++ breedFetcherRefactor.js
@@ -11,28 +11,11 @@ const fetchBreedDescription = (breedName, callback) => {
     }
     const breeds = JSON.parse(body); // parse the body string into a JSON array
     if (breeds.length > 0) {
-      console.log(breeds.length);
-      const descriptionArray = []; // accumulator
-      for (let i = 0; i < breeds.length - 2; i++) {
-        descriptionArray.push(breeds[i].description);
-      }
-      callback(null, descriptionArray); // pass the data into the data parameter of the callback
+      callback(null, breeds[0].description); // pass the data into the data parameter of the callback
     } else {
-      callback("No cats by that name here!");
+      callback(null, "No cats by that name here!");
     }
   });
 };
 
 module.exports = fetchBreedDescription;
-
-// const parse = (error, response, body) => {
-//   // pull the JSON string from the next
-//   if (error) throw error;
-//   let data = JSON.parse(body); // declare data equal to the parsed string
-//   if (data.length === 0) {
-//     // check that there is something in the data array
-//     console.log("No cats by that name here!");
-//   } else {
-//     console.log(data); // print the data string to the console
-//   }
-// };
